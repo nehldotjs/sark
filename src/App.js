dddfbe753ddb4d8a17499eeb0627e68c97017582
nehldotjs <nelsonosuya11@gmail.com>
2024-08-19 23:09:18 +0100

    NEW COMMIT

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>hey</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>SARK</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
